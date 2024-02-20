ebc6d3cb31462e7f6cccb219bbb2788877222e24
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2024-02-20 11:11:41 -0300

    comencé la actividad 3 de jab. Falta ordenamiento de mayor a menor por estrellas recibidad.

---
+++ curso-jab/actividad3-jab/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import datos from './datos'
+import { useEffect } from 'react';
+import estrella from './images/estrella.png'
+
+function App() {
+  
+  const [datosAlimentos, setDatosAlimentos] = useState(datos);
+  const [productosSeleccionados, setProductosSeleccionados] = useState([]);
+
+  const nuevosDatos = datosAlimentos.filter((e) => {
+    return e.estado === false
+  });
+
+  const agregarDato = (id) => {
+    const nuevoDato = datosAlimentos.find(e => e.id === id);
+    if(nuevoDato){
+      nuevoDato.estado = true;
+      setDatosAlimentos([...datosAlimentos]);
+    }
+
+  }
+
+  useEffect(() => {
+    let nuevoProducto = datosAlimentos.filter((e) => {
+      return e.estado === true
+    });
+
+    nuevoProducto = nuevoProducto.map(producto => ({
+      ...producto,
+      estrellas: 1
+    }));
+
+    setProductosSeleccionados(nuevoProducto);
+    
+  }, [datosAlimentos])
+
+  const agregarEstrella = (producto) => {
+    const nuevaEstrella = productosSeleccionados.find(e => e.id === producto.id);
+    console.log(nuevaEstrella)
+    if(nuevaEstrella){
+      if(nuevaEstrella.estrellas < 5){
+        nuevaEstrella.estrellas = nuevaEstrella.estrellas+1;
+      }else{
+        nuevaEstrella.estrellas = 1;
+      }
+
+      setProductosSeleccionados([...productosSeleccionados]);
+    }
+  }
+
+  return (
+    <>  
+      <div className='container'>
+        {
+          nuevosDatos.map(element => (
+            <div key={element.id} className='productos' onClick={() => agregarDato(element.id)}>
+              {element.producto}
+            </div>
+          ))
+        }
+      </div>
+
+      <div className='container-seleccionados'>
+        {
+          productosSeleccionados.map((element, index) => (
+            <div key={index} className='productos-seleccionados' onClick={() => agregarEstrella(element)}>
+                {element.producto}
+                <div className='estrellas-img'>
+                  {[...Array(element.estrellas)].map((_, i) => (
+                    <img key={i} src={estrella} alt="estrella" />
+                  ))}
+                </div>
+            </div>
+          ))
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ curso-jab/actividad3-jab/src/datos.jsx
@@ -0,0 +1,29 @@
+const datos=[
+  {
+    producto: "Hamburguesa doble",
+    estado:false,
+    id:1
+  },
+  {
+    producto: "Filete de ternera",
+    estado:false,
+    id:2
+  },
+  {
+    producto: "Ensalada de boniatos",
+    estado:false,
+    id:3
+  },
+  {
+    producto: "Lenguado a la plancha",
+    estado:false,
+    id:4
+  },
+  {
+    producto: "Macarrones boloñesa",
+    estado:false,
+    id:5
+  }
+]
+
+export default datos
